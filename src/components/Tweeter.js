241d9fc319c443c3cca37356dbe308c1fd67ec9e
GautamPB <pbg2402@gmail.com>
2021-01-23 13:22:04 +0530

    Added tweeter

--- src/components/Tweeter.js
+++ src/components/Tweeter.js
@@ -1,6 +1,11 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, Button } from '@material-ui/core'
 import React from 'react'
 import '../styles/Tweeter.css'
+import PhotoIcon from '@material-ui/icons/InsertPhotoOutlined'
+import GifIcon from '@material-ui/icons/Gif'
+import PollIcon from '@material-ui/icons/Poll'
+import EmojiIcon from '@material-ui/icons/EmojiEmotionsOutlined'
+import CalendarIcon from '@material-ui/icons/CalendarTodayOutlined'
 
 function Tweeter() {
     return (
@@ -10,6 +15,16 @@ function Tweeter() {
             <div className="tweeter__top">
                 <Avatar className="tweeter__profilePic" />
                 <input type="text" placeholder="What's happening?" />
+            </div>
+
+            <div className="tweeter__bottom">
+                <PhotoIcon className="tweeter__option" />
+                <GifIcon className="tweeter__option" />
+                <PollIcon className="tweeter__option" />
+                <EmojiIcon className="tweeter__option" />
+                <CalendarIcon className="tweeter__option" />
+
+                <Button className="tweeter__button">Tweet</Button>
             </div>
         </div>
     )
